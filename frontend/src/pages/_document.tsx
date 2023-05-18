9ac47c15982779591919cb5d22e04779f306e643
Shubham0850 <rajshubham0850@gmail.com>
2023-05-18 14:20:15 +0530

    import google font

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title='Counter dApp' />
+      <Head title="Counter dApp" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700;800&display=swap"
+        rel="stylesheet"
+      />
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
